f7ee08f5c5a32263fa7a2d539777be21d56a753d
nishant <vikaskumar07.dev@gmail.com>
2022-07-09 20:47:21 +0530

    first commit

---
+++ resources/js/Shared/AdminLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Admin Dashboard</title>
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_navbar.html -->
+      <AdminNavVue />
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        <AdminSidebarVue />
+        <!-- partial -->
+        <slot />
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+  </body>
+</template>
+<script>
+import {Link} from '@inertiajs/inertia-vue3';
+import AdminSidebarVue from './AdminSidebar.vue';
+import AdminNavVue from './AdminNavbar.vue';
+
+export default {
+    components: { Link, AdminSidebarVue, AdminNavVue },
+};
+</script>
